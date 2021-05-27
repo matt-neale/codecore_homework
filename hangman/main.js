4047a857ebe85e872762f8440e3ddf152c1640cd
Matt Neale <mneale87@gmail.com>
2021-05-26 23:07:37 -0700

    added guess lines, letters become visible after a correct guesses

--- hangman/main.js
+++ hangman/main.js
@@ -1,14 +1,39 @@
 $(function () {
+  $("body").append(`<div class='container1'></div>`);
   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-
   alphabet.forEach((node) => {
-    $("body").append(`<div class="letter container"> ${node} </div>`);
+    $(".container1").append(`<div class="letter"> ${node} </div>`);
   });
 
-  document.querySelectorAll(".letter").forEach((node) => {
-    node.addEventListener("click", function () {
-      node.classList.add("selected");
-      node.classList.add("highlight");
+  $("body").append(`<div class='container2'></div>`);
+
+  const answer = "stranger".split("");
+  answer.forEach((node) => {
+    $(".container2").append(`<div class="guess"> ${node} </div>`);
+  });
+
+  $("body").append(`<div class='container3'></div>`);
+
+  answer.forEach((node) => {
+    $(".container3").append(`<div class='underline'></div>`);
+  });
+
+  $("body").append(`<img src="images/gallows.jpg">`);
+
+  document.querySelectorAll(".letter").forEach((letter) => {
+    letter.addEventListener("click", function () {
+      letter.classList.add("selected");
+      letter.classList.add("highlight");
+      console.log(letter.innerHTML);
+
+      document.querySelectorAll(".guess").forEach((guess) => {
+        if (letter.innerHTML === guess.innerHTML) {
+          console.log(guess.innerHTML);
+          guess.style.visibility = "visible";
+        }
+      });
     });
   });
+
+  function hangman() {}
 });
